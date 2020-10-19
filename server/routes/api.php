bd0d36026ea251a96cdd453de2c712019d9903e3
leyon00 <leyonik995@gmail.com>
2020-10-19 16:48:25 +0600

    put route in api.php and created controllers

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PortfolioController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resources([
+    'user' => UserController::class,
+    'user/portfolio' => PortfolioController::class,
+]);
