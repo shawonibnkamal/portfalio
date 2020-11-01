97d82527fbc98238bc2843d0588e7f9c4178f269
leyon00 <leyonik995@gmail.com>
2020-11-01 20:10:45 +0600

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,21 @@ import Footer from './footer';
 
 function App() {
 
-  /*
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("login_token") ? true : false);
+
   axios.post(process.env.REACT_APP_API_URL + "api/login/user", {} , {
     headers: {
       "authorization": "Bearer " + localStorage.getItem("login_token")
   }}).then(
     res => {
-
+      console.log("login token valid");
     }
-  ).catch( error => console.log(error.response.data));
-  */
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("login_token") ? true : false);
+  ).catch(error => {
+    console.log(error.response.data);
+    localStorage.clear();
+    setLoggedIn(false);
+  });
+
 
   console.log("Welcome to Portfal.io");
 
