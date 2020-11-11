eccd9f0cd30224daaead02beed83a4f83b062d9e
leyon00 <leyonik995@gmail.com>
2020-11-11 22:55:09 +0600

    bug fix

--- client/src/portfolioGallery.js
+++ client/src/portfolioGallery.js
@@ -4,7 +4,7 @@ function PortfolioGallery({ userPortfolio }) {
 
   return (
     <div className="border border-black col-4 m-2 p-2">
-      <img src={userPortfolio.profile_pic} alt="portfolio pic" width="400" height="400" /> <br />
+      <img className="bg-dark" src={userPortfolio.profile_pic} alt="portfolio pic" width="400" height="400" /> <br />
       <p>
         Name: {userPortfolio.name} <br />
         Description: {userPortfolio.description}<br />
