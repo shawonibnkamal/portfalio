315f9886ae322c5427630e788fe889e5f6c4f844
leyon00 <leyonik995@gmail.com>
2020-11-13 12:26:48 +0600

    added popup image in portfolio

--- client/src/portfolioGallery.js
+++ client/src/portfolioGallery.js
@@ -6,7 +6,21 @@ function PortfolioGallery({ userPortfolio }) {
 
   return (
     <div className="border border-black col-4 m-2 p-2">
-      <img className="img-fluid" src={userPortfolio.profile_pic ? userPortfolio.profile_pic : defaultPortfolioPic} alt="portfolio pic"  /> <br /><br />
+
+      <img data-toggle="modal" data-target="#myModal" className="img-fluid" src={userPortfolio.profile_pic ? userPortfolio.profile_pic : defaultPortfolioPic} alt="portfolio pic" /> <br /><br />
+      <div className="modal fade" id="myModal" role="dialog">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-body">
+            <img className="img-fluid w-100 h-100" src={userPortfolio.profile_pic ? userPortfolio.profile_pic : defaultPortfolioPic} alt="portfolio pic" />
+            </div>
+            <div className="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <p>
         Name: {userPortfolio.name} <br />
         Description: {userPortfolio.description}<br />
