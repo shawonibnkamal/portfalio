7d4d9fcb56c99b6061557399c0b1bb62a54a2c8b
Shawon Kamal <shawon.1165@gmail.com>
2020-11-22 04:30:10 -0330

    fix case sensitivity

---
+++ client/src/Navbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function Navbar({ loggedIn, setLoggedIn }) {
+  const handleLogout = (e) => {
+    axios
+      .post(
+        process.env.REACT_APP_API_URL + "api/logout",
+        {},
+        {
+          headers: {
+            authorization: "Bearer " + localStorage.getItem("login_token"),
+          },
+        }
+      )
+      .then((res) => {
+        //console.log("logging out token> " + localStorage.getItem("login_token"));
+        //console.log(res.data);
+        localStorage.removeItem("login_token");
+        //console.log("after remove token> " + localStorage.getItem("login_token"));
+
+        setLoggedIn(false);
+      })
+      .catch((error) => console.log(error.response.data));
+  };
+
+  return (
+    <div className="navbar text-dark">
+      <Link to="/">
+        <h1 className="text-dark navbar-brand text-monospace">Portfal.io</h1>{" "}
+      </Link>
+      <div className="mr-right">
+        {loggedIn ? (
+          <div>
+            <a href="/">
+              <button className="btn btn-light mr-2">Dashboard</button>
+            </a>
+            <a href="/settings">
+              <button className="btn btn-light mr-2">Settings</button>
+            </a>
+            <button className="btn btn-light mr-2" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div>
+            <Link className="text-dark btn" to="/login">
+              {" "}
+              Login{" "}
+            </Link>
+            <Link className="btn btn-outline-info" to="/signup">
+              {" "}
+              Sign Up{" "}
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
